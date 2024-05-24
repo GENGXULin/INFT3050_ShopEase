b6c94162d77b3f12e61db6a37171094a3608f265
JAKCYXG <105270761+JAKCYXG@users.noreply.github.com>
2024-05-24 12:53:35 +0800

    添加项目文件。

---
+++ INFT3050_ShopEase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INFT3050_ShopEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
